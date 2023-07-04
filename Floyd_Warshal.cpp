dc9c8e5acdbd9231ff11e53f8490a55fe458dcbd
Paras <paras.nagpal743@gmail.com>
2023-07-04 18:32:12 +0530

    Update Floyd_Warshal.cpp

--- Floyd_Warshal.cpp
+++ Floyd_Warshal.cpp
@@ -1,71 +1,21 @@
-//{ Driver Code Starts
-//Initial template for C++
-
-#include<bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-//User function template for C++
-
-class Solution {
-  public:
-	void shortest_distance(vector<vector<int>>&matrix){
-	     int n = matrix.size()  ;
-	     
-	     for(int i=0 ; i<n ; ++i){
-	         for(int j=0 ; j<n ; ++j){
-	             if(matrix[i][j] == -1){
-	                 matrix[i][j] = 1e9 ;
-	             }
-	             if(i == j) matrix[i][j] = 0 ;
-	         }
-	     }
-	     
-	     for(int k=0 ; k < n ; ++k){
-	         for(int i=0 ; i<n ; ++i){
-	             for(int j=0 ; j<n ; ++j){
-	                 matrix[i][j] = min(matrix[i][j] , matrix[i][k] + matrix[k][j]);
-	             }
-	         }
-	     }
-	     
-	     for(int i=0 ; i<n ; ++i){
-	         for(int j=0 ; j<n ; ++j){
-	             if(matrix[i][j] == 1e9){
-	                 matrix[i][j] = -1 ;
-	             }
-	         }
-	     }
-	     
-	     
-	     
-	     
-	     
-	}
-};
-
-//{ Driver Code Starts.
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int n;
-		cin >> n;
-		vector<vector<int>>matrix(n, vector<int>(n, -1));
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < n; j++){
-				cin >> matrix[i][j];
-			}
-		}
-		Solution obj;
-		obj.shortest_distance(matrix);
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < n; j++){
-				cout << matrix[i][j] << " ";
-			}
-			cout << "\n";
-		}
-	}
-	return 0;
+void FloydWarshall(int n, vector<pair<int, ll>> *adj, vector<vector<ll>> &dist) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            dist[i][j] = INF;
+        }
+        dist[i][i] = 0;
+    }
+    for (int i = 0; i < n; i++) {
+        for (auto j : adj[i]) {
+            dist[i][j.ff] = min(dist[i][j.ff], j.ss);
+        }
+    }
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dist[i][k] < INF && dist[k][j] < INF) // for updating only when there is a path
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
+    }
 }
-// } Driver Code Ends
